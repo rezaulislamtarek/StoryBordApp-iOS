6a4c7c626715f719217260842818196bc8c57ee4
rezaulislam <mrezaulislam.t@gmail.com>
2024-05-30 00:15:02 +0600

    IBOutlet IBAction
    change image based on button click

--- StoryBordApp iOS/ViewController.swift
+++ StoryBordApp iOS/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changePhotoPressed(_ sender: UIButton) {
+        if userImage.image == UIImage(named: "tarek_new"){
+            userImage.image = UIImage(named: "tarek_two")
+        }else{
+            userImage.image = UIImage(named: "tarek_new")
+        }
+        
+    }
+    
 }
 
